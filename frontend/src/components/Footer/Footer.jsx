591653a805c5e7bb952a819b37d1777869a80482
ManojKumarMoka <21131a05c3@gvpce.ac.in>
2024-09-01 10:42:30 +0530

    Frontend

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,18 +4,18 @@ import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div className="footer" id = "footer">
-        <div className="footer-content">
-          <div className="footer-content-left">
-             <img src={assets.logo} alt="" />
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iste ea accusantium quibusdam placeat nobis, neque accusamus culpa voluptas aut atque sit modi facere illo voluptatum numquam voluptates provident. Optio.</p>
-             <div className="footer-social-icons">
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
                 <img src={assets.linkedin_icon} alt="" />
-             </div>
-          </div>
-          <div className="footer-content-center">
+            </div>
+        </div>
+        <div className="footer-content-center">
             <h2>COMPANY</h2>
             <ul>
                 <li>Home</li>
@@ -23,19 +23,17 @@ const Footer = () => {
                 <li>Delivery</li>
                 <li>Privacy policy</li>
             </ul>
-          </div>
-          <div className="footer-content-right">
+        </div>
+        <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-221-456-7890</li>
+                <li>+1-212-456-7890</li>
                 <li>contact@tomato.com</li>
             </ul>
-          </div>
         </div>
-        <hr />
-        <p className="footer-copyright">
-            Copyright 2024 © Tomato.com - All Right Reserved.
-        </p>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
     </div>
   )
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className="header-contents">
-            <h2>Order delicious food</h2>
-            <p>Whether you're in the mood for a hearty meal, a light snack, or something unique, our diverse menu ensures you'll find exactly what you're looking for. Enjoy the convenience of discovering new culinary delights and satisfying your cravings with just a few clicks..
-            </p>
-            <button>View Menu</button>
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your favourite food here</h2>
+                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <button>View Menu</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
